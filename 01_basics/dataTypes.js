3a742cbdf521eb5e19f4b853154a019fb8975db7
Saswata Kumar Mishra <saswatamishra828@gmail.com>
2025-10-22 19:21:48 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//teret all js code as newer version
+
+//alert(3+3)//we are using node js not browser
+
+console.log(3+3)
+let age=18
+let isLoggedIn = false
+let state;
+//number ->2 to power of 53
+//bigint
+//boolean
+//string
+//undefined
+//symbol->unique
+
+//object
+console.log(typeof age)//number
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
