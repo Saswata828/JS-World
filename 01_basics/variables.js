c449f4db6923a68c22a261b23c81cc91caae8ca3
Saswata Kumar Mishra <saswatamishra828@gmail.com>
2025-10-23 00:13:40 +0530

    Add account variables and update email/password
    Initialize account variables and update values.

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1445623
+let accountEmail = "saswta@gmail.com"
+var accountPassword = 12542
+accountCity = "kalahandi"
+let accountState;
+
+accountEmail="raj@gmail.com"
+accountPassword=124985
+accountCity="bbsr"
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState]) 
