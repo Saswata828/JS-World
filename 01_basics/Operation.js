881518602589e74a86673e28e0b8d584fb4693fa
Saswata Kumar Mishra <saswatamishra828@gmail.com>
2025-10-22 20:10:58 +0000

    Operation

---
+++ 01_basics/Operation.js
@@ -0,0 +1,26 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//33->33
+//"33abc"->NaN
+//true->1; false->0
+
+let isboolean = "saswata"
+
+let booleanIsBoolean = Boolean(isboolean)
+console.log(booleanIsBoolean)
+//1->true , 0->false
+//""->fasle
+//"saswata"->true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
